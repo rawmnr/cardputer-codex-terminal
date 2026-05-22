a3ff24849ccdd156dcc664fdc29b28288ba67bff
Romain Meunier <60362278+rawmnr@users.noreply.github.com>
2026-05-22 08:07:56 +0200

    Guard native build mingw setup

--- firmware/tools/setup_mingw.py
+++ firmware/tools/setup_mingw.py
@@ -1,16 +1,18 @@
 Import("env")
 import os
 
-# Force GCC/G++ from MSYS2
+# Force GCC/G++ from MSYS2 when available.
 mingw_bin = r"C:\tools\msys64\mingw64\bin"
-env.Replace(
-    CC=os.path.join(mingw_bin, "gcc.exe"),
-    CXX=os.path.join(mingw_bin, "g++.exe"),
-    LINK=os.path.join(mingw_bin, "g++.exe"),
-    AR=os.path.join(mingw_bin, "ar.exe"),
-    RANLIB=os.path.join(mingw_bin, "ranlib.exe")
-)
 
-# Add mingw to path for sub-processes
-env.PrependENVPath("PATH", mingw_bin)
-env.PrependENVPath("PATH", r"C:\tools\msys64\usr\bin")
+if os.path.isdir(mingw_bin):
+    env.Replace(
+        CC=os.path.join(mingw_bin, "gcc.exe"),
+        CXX=os.path.join(mingw_bin, "g++.exe"),
+        LINK=os.path.join(mingw_bin, "g++.exe"),
+        AR=os.path.join(mingw_bin, "ar.exe"),
+        RANLIB=os.path.join(mingw_bin, "ranlib.exe"),
+    )
+
+    # Add mingw to path for sub-processes.
+    env.PrependENVPath("PATH", mingw_bin)
+    env.PrependENVPath("PATH", r"C:\tools\msys64\usr\bin")
